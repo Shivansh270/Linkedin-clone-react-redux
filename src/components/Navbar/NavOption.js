15db2b94cf7ad111fe87e98ab4e534f29166df16
Shivansh270 <gupta.shivansh2709@gmail.com>
2023-07-09 12:29:55 +0530

    stared the build and created navbar and sidebar

--- src/components/Navbar/NavOption.js
+++ src/components/Navbar/NavOption.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from './Navbar'
 import "./NavOption.css"
 import { Avatar } from '@mui/material';
 
@@ -9,7 +8,7 @@ const NavOption = ({title, Icon, avatar}) => {
         {Icon && <Icon className="nav-option-icon" />}
         {avatar && <Avatar className="nav-option-icon" src={avatar} />}
         <h3 className="nav-option-title">{title}</h3>
-    </div>
+    </div> 
   )
 }
 
